3e545a9cea9e1d6d38fe882233292b2a8fd31d23
the-wonderful-andy <46044960+the-wonderful-andy@users.noreply.github.com>
2020-06-24 02:49:14 +0800

    general develop

--- design/top.v
+++ design/top.v
@@ -21,6 +21,47 @@
 
 
 module top(
-
+	input hclk, 
+	output jump, branch, alusrc, memwrite, 
+	memtoreg, regwrite, regdst, 
+	output [2:0]Alucontrol
     );
+
+wire lclk;
+wire [31:0]nextInstAddr, instAddr;
+wire [31:0]inst; //instruction
+
+Clk_div #(
+	.HALF_DIV(5)
+	)
+	div(
+	.hclk(hckl), 
+	.lclk(lclk)
+	);
+
+PC_32b pc(
+	.clk(lclk), 
+	.clr(1'b0), 
+	.addr_in(nextInstAddr), 
+	.addr_out(instAddr)
+	);
+
+PC_adder_32b padder(
+	.addr_in(instAddr), 
+	.offset(4), 
+	.addr_out(nextInstAddr)
+	);
+
+instruction_mem instmem(
+  .clka(lclk),    // input wire clka
+  .ena(1),      // input wire ena
+  .wea(0),      // input wire [0 : 0] wea
+  .addra(instAddr[9:0]),  // input wire [9 : 0] addra
+  .dina(32'b0),    // input wire [31 : 0] dina
+  .douta(inst)  // output wire [31 : 0] douta
+);
+
+
+
+
 endmodule
